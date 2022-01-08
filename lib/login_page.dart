45ba5e3d9b2b6fa36620417569328f7c5ab6aee7
randolfo <randolfo.ufg@gmail.com>
2022-01-08 08:35:05 -0300

    info

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,26 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import 'google_sign_in.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String appName = 'Fazendo leitura....';
+  String packageName = 'Fazendo leitura....';
+  String version = 'Fazendo leitura....';
+  String buildNumber = 'Fazendo leitura....';
+
+  @override
+  void initState() {
+    PackageInfo.fromPlatform().then((PackageInfo packageInfo) {
+      setState(() {
+        appName = packageInfo.appName;
+        packageName = packageInfo.packageName;
+        version = packageInfo.version;
+        buildNumber = packageInfo.buildNumber;
+      });
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(title),
+        title: Text(widget.title,
+            style: Theme.of(context).appBarTheme.titleTextStyle),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.info),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('Sobre'),
+                  content: Card(
+                    elevation: 1.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: '$appName\n',
+                            style: Theme.of(context).textTheme.headline4,
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            text: 'Versão: \n',
+                            style: Theme.of(context).textTheme.headline4,
+                            children: <TextSpan>[
+                              TextSpan(
+                                  text: '$version\n\n',
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold)),
+                              TextSpan(text: 'Build: $buildNumber'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      child: Text('OK',
+                          style: Theme.of(context).textTheme.headline4),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -35,10 +102,6 @@ class LoginPage extends StatelessWidget {
                 provider.googleLogin();
               },
               child: const Text('Usar Gmail'),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Informar meu nome'),
             ),
           ],
         ),
